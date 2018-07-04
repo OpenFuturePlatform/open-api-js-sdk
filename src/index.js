ad7bacf1605b369318d0cd73d43e27eced24ee1c
Pavel Anosov <pavel.anosov@zensoft.io>
2018-07-04 10:28:39 +0300

    join in to single object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,148 @@
 import 'babel-polyfill';
+import {api} from './configs';
+import {addressValidation} from './helpers/validators';
+import Paths from './dicts/paths';
 
-import Scaffold from './services/scaffold';
-import ShareHolder from './services/share-holder';
+export default class OpenJs {
+  constructor(openKey = '') {
+    this.api = api(openKey);
+  }
 
+  async getScaffolds() {
+    try {
+      const result = await this.api.get(Paths.Scaffold.GetAll);
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
 
-export {Scaffold, ShareHolder};
+  async getScaffold(address) {
+    const errors = addressValidation(address);
+    if (errors.length !==0) {
+      return errors;
+    };
+    try {
+      const result = await this.api.get(Paths.Scaffold.GetItem(address));
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async getSummary(address) {
+    const errors = addressValidation(address);
+    if (errors.length !==0) {
+      return errors;
+    };
+    try {
+      const result = await this.api.get(Paths.Scaffold.GetSummary(address));
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async getTransactions(address) {
+    const errors = addressValidation(address);
+    if (errors.length !==0) {
+      return errors;
+    };
+    try {
+      const result = await this.api.get(Paths.Scaffold.GetTransactions(address));
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async deployScaffold(data) {
+    try {
+      const result = await this.api.post(Paths.Scaffold.Deploy, data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async setWebhook(address, data) {
+    const errors = addressValidation(address);
+    if (errors.length !==0) {
+      return errors;
+    };
+    try {
+      const result = await this.api.patch(Paths.Scaffold.SetWebhook(address), data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async getQuota() {
+    try {
+      const result = await this.api.get(Paths.Scaffold.GetQuota);
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async addShareHolder(address, data) {
+    const errors = addressValidation(address);
+    if (errors.length !==0) {
+      return errors;
+    };
+    try {
+      const result = await this.api.post(Paths.ShareHolder.Add(address), data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async updateShareHolder(address, data) {
+    const errors = addressValidation(address);
+    if (errors.length !==0) {
+      return errors;
+    };
+    try {
+      const result = await this.api.put(Paths.ShareHolder.Update(address), data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  async removeShareHolder(address, data) {
+    const errors = addressValidation(address);
+    if (errors.length !==0) {
+      return errors;
+    };
+    try {
+      const result = await this.api.delete(Paths.ShareHolder.Remove(address), data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      return result.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,10 @@
 import {expect} from 'chai';
-import {Scaffold} from '../src/';
+import OpenJs from '../src/';
 
 const OPEN_KEY = 'op_pk_c681dfb2-7add-4e67-80e3-4fe53969e1c7';
 const SCAFFOLD_ADDRESS = '0xc013d4c3a6ec409722684c296a36a90e187c6959';
 
-const request = new Scaffold(OPEN_KEY)
+const request = new OpenJs(OPEN_KEY)
 
 it("Get all scaffolds", async function() {
   const result = await request.getScaffolds();
@@ -96,4 +96,17 @@ it("Get quota", async function() {
   expect(result).to.have.property('limitCount');
   expect(result.currentCount).to.be.a('number');
   expect(result.limitCount).to.be.a('number');
+});
+
+it("Add share holder", async function() {
+  const data = {
+    "address": '0x33c2fb41aed258bf77c52674db552cf5625017d6',
+    "percent": 30
+  };
+  const result = await request.addShareHolder(SCAFFOLD_ADDRESS, data);
+  console.log('share holder', result);
+
+  expect(result).to.be.a('object');
+
+  expect(result).not.to.have.property('error');
 });
